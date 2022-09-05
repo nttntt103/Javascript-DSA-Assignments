b23f062d21d09cd12ba8062c684308ca729f3b3a
nttntt103 <70335354+nttntt103@users.noreply.github.com>
2022-09-05 23:13:00 +0700

    Update main.js

--- main.js
+++ main.js
@@ -5,9 +5,9 @@ function isIncreasingNumber(n) {
   const sortArr = numberStr.split("").sort((a, b) => {
     return a - b;
   });
-  console.log("isIncreasingNumber:", numberStr === sortArr.join(""), n);
+  return numberStr === sortArr.join("");
 }
-isIncreasingNumber(167);
+console.log("isIncreasingNumber:", isIncreasingNumber(167));
 
 // NUMBER 02 - KIỂM TRA SỐ TĂNG DẦN ĐỀU THEO KHOẢNG CÁCH CHO TRƯỚC
 
@@ -17,25 +17,14 @@ function isIncreasingNumberByDistance(n, x) {
     return a - b;
   });
   const sortArrLength = sortArr.length;
-
-  if (numberStr !== sortArr.join("")) {
-    console.log(`isIncreasingNumberByDistance (${x}):`, false, n);
-  } else {
-    const isResultNumber =
-      sortArr
-        .map((elem, index) => elem - x * index)
-        .reduce((acc, elem) => acc + elem) ===
-      sortArr[0] * sortArr.length;
-    console.log(`isIncreasingNumberByDistance (${x}):`, isResultNumber, n);
+  for (let i = 0; i < sortArrLength; i++) {
+    if (Number(sortArr[i]) + x !== Number(sortArr[i + 1])) {
+      return false;
+    }
+    return true;
   }
-  // const elemNumbers = (sortArr[sortArrLength - 1] - sortArr[0]) / x + 1;
-  // console.log(
-  //   `isIncreasingNumberByDistance (${x}):`,
-  //   numberStr === sortArr.join("") && elemNumbers === sortArrLength,
-  //   n
-  // );
 }
-isIncreasingNumberByDistance(135, 2);
+console.log("isIncreasingNumberByDistance:", isIncreasingNumberByDistance(135, 2));
 
 // NUMBER 04 - KIỂM TRA SỐ NGUYÊN TỐ
 
@@ -44,16 +33,16 @@ function isPrime(n) {
   for (let i = 1; i <= n; i++) {
     n % i === 0 && divisorArr.push(i);
   }
-  console.log("isPrime:", divisorArr.length === 2, n);
+  return divisorArr.length === 2;
 }
-isPrime(129);
+console.log("isPrime:", isPrime(9));
 
 // NUMBER 05 - KIỂM TRA SỐ CHÍNH PHƯƠNG
 
 function isPerfectSquare(n) {
-  console.log("isPerfectSquare:", Math.round(Math.sqrt(n)) === Math.sqrt(n), n);
+  return Math.round(Math.sqrt(n)) === Math.sqrt(n);
 }
-isPerfectSquare(5);
+console.log("isPerfectSquare:", isPerfectSquare(4));
 
 // NUMBER 06 - KIỂM TRA SỐ HOÀN HẢO
 
@@ -64,36 +53,23 @@ function isPerfectNumber(n) {
       sum += i;
     }
   }
-  console.log("isPerfectNumber:", n === sum, n);
+  return n === sum;
 }
-isPerfectNumber(6);
+console.log("isPerfectNumber:", isPerfectNumber(6));
 
 // NUMBER 07 - KIỂM TRA SỐ ĐỐI XỨNG
 
 function isSymetricNumber(n) {
-  console.log(
-    "isSymetricNumber:",
-    n.toString() === n.toString().split("").reverse().join(""),
-    n
-  );
+  return n.toString() === n.toString().split("").reverse().join("");
 }
-isSymetricNumber(121);
+console.log("isSymetricNumber:", isSymetricNumber(121));
 
 // NUMBER 08 - KIỂM TRA SỐ CÓ TỔNG CHỮ SỐ CHIA HẾT CHO 10
 
 function isDivisibleBy10(n) {
-  console.log(
-    "isDivisibleBy10:",
-    n
-      .toString()
-      .split("")
-      .reduce((acc, elem) => Number(acc) + Number(elem)) %
-      10 ===
-      0,
-    n
-  );
+  return n.toString().split("").reduce((acc, elem) => Number(acc) + Number(elem)) % 10 === 0;
 }
-isDivisibleBy10(145);
+console.log("isDivisibleBy10:", isDivisibleBy10(145));
 
 // STRING 01 - ĐẾM SỐ TỪ CÓ TRONG CÂU
 
@@ -104,26 +80,13 @@ function countWord(str) {
   for (let i = 0; i < arrLength; i++) {
     arr[i] !== "" && count++;
   }
-  console.log("countWord:", count, arr, str);
+  return count;
 }
-countWord("Holy  moly  ");
+console.log("countWord:", countWord("Holy  moly  "));
 
 // STRING 02 - THỐNG KÊ SỐ LƯỢNG TỪ TRONG CÂU
 
 function statisticsWord(str) {
-  // const outputMap = new Map();
-  // const arr = str.split(" ");
-  // const strSet = new Set(arr);
-  // const arrLength = arr.length;
-  // for (let i = 0; i < arrLength; i++) {
-  //   if (arr[i] !== "" && strSet.has(arr[i])) {
-  //     let count = 1;
-  //     outputMap.get(arr[i]) === undefined
-  //       ? outputMap.set(arr[i], count)
-  //       : outputMap.set(arr[i], outputMap.get(arr[i])++);
-  //   }
-  // }
-  // console.log(str, outputMap);
   const arr = str.split(" ");
   //const arr = str.replace(/ /g, '').split('')
   const arrLength = arr.length;
@@ -136,9 +99,9 @@ function statisticsWord(str) {
     outputObject[char]++;
   }
   delete outputObject[" "];
-  console.log("statisticsWord:", outputObject, str);
+  return outputObject;
 }
-statisticsWord("Captain Teemo on duty - Teemo");
+console.log("statisticsWord:", statisticsWord("Captain Teemo on duty - Teemo"));
 
 // STRING 03 - THỐNG KÊ KÝ TỰ CÓ TRONG CÂU
 
@@ -154,39 +117,24 @@ function statisticsCharacter(str) {
   }
   outputObject["spaces"] = outputObject[" "];
   delete outputObject[" "];
-  console.log("statisticsCharacter:", outputObject, str);
+  return outputObject;
 }
-statisticsCharacter("Trust nothing but your strength - Sejuani");
+console.log("statisticsCharacter:", statisticsCharacter("Trust nothing but your strength - Sejuani"));
 
 // ARRAY CHECK 09 - KIỂM TRA MẢNG CÓ TĂNG DẦN HAY KHÔNG
 
 function isIncreasingNumberList(numberList) {
   const strFromArray = numberList.join("");
-  console.log(
-    "isIncreasingNumberList:",
-    strFromArray.length >= 2 &&
-      strFromArray ===
-        [...numberList]
-          .sort((a, b) => {
-            return a - b;
-          })
-          .join(""),
-    numberList
-  );
+  return strFromArray.length >= 2 && strFromArray === [...numberList].sort((a, b) => a - b).join("");
 }
-isIncreasingNumberList([1, 2]);
+console.log("isIncreasingNumberList:", isIncreasingNumberList([1, 2]));
 
 // ARRAY CHECK 11 - KIỂM TRA MẢNG CÓ ĐỐI XỨNG KHÔNG
 
 function isSymetricList(numberList) {
-  console.log(
-    "isSymetricList:",
-    numberList.length >= 1 &&
-      numberList.join("") === [...numberList].reverse().join(""),
-    numberList
-  );
+  return numberList.length >= 1 && numberList.join("") === [...numberList].reverse().join("");
 }
-isSymetricList([1, 3, 5, 3, 2]);
+console.log("isSymetricList:", isSymetricList([1, 3, 5, 3, 2]));
 
 // ARRAY CHECK 12 - KIỂM TRA MẢNG CÓ SỐ FIBONACI NHỎ HƠN 100 HAY KHÔNG (F0 = 0, F1 = 1, Fn = Fn-1 + Fn-2)
 
@@ -204,24 +152,18 @@ function hasFibonaciNumber(numberList) {
   const fibonaciSet = new Set(fibonaciNumbers);
   const inputSet = new Set(numberList);
   const mergedSet = new Set([...inputSet, ...fibonaciSet]);
-  console.log(
-    "hasFibonaciNumber:",
-    inputSet.size + fibonaciSet.size > mergedSet.size,
-    numberList
-  );
+  return inputSet.size + fibonaciSet.size > mergedSet.size;
 }
-hasFibonaciNumber([1, 3, 5, 7, 9]);
+console.log("hasFibonaciNumber:", hasFibonaciNumber([1, 3, 5, 7, 9]));
 
 // ARRAY FIND 5 - TÌM SỐ LỚN THỨ 2 TRONG MẢNG
 
 function findSecondLargestNumber(numberList) {
-  console.log(
-    "findSecondLargestNumber:",
-    numberList.sort((b, a) => a - b)[1],
-    numberList
-  );
+  const sortArr = numberList.sort((b, a) => a - b);
+  const arrToSet = new Set(sortArr);
+  return [...arrToSet][1];
 }
-findSecondLargestNumber([10, 0, 8, 20]);
+console.log("findSecondLargestNumber:", findSecondLargestNumber([10, 0, 8, 20, 20]));
 
 // ARRAY FIND 6 - TÌM SỐ CHÍNH PHƯƠNG CUỐI CÙNG TRONG MẢNG
 
@@ -232,9 +174,9 @@ function findLastPerfectSquare(numberList) {
     Math.round(Math.sqrt(numberList[i])) === Math.sqrt(numberList[i]) &&
       squareNumbers.push(numberList[i]);
   }
-  console.log("findLastPerfectSquare:", squareNumbers.sort((b, a) => a - b)[0]);
+  return squareNumbers[squareNumbers.length - 1];
 }
-findLastPerfectSquare([1, 7, 9, 16, 27]);
+console.log("findLastPerfectSquare:", findLastPerfectSquare([1, 7, 9, 16, 27, 4]));
 
 // ARRAY COUNT 5 - ĐẾM SỐ LƯỢNG CÁC SỐ CÓ TRONG MẢNG A MÀ KHÔNG CÓ TRONG MẢNG B
 
@@ -245,35 +187,15 @@ function countNumbersNotInB(a, b) {
   for (let i = 0; i < aLength; i++) {
     !bSet.has(a[i]) && count++;
   }
-  console.log("countNumbersNotInB:", count, a, b);
-
-  // const aSet = new Set(a);
-  // const bSet = new Set(b);
-  // console.log(bSet)
-  // const mergedSet = new Set([...bSet, ...aSet]);
-  // const halfMergedSet = new Set([...bSet, ...a]);
-  // console.log(halfMergedSet)
-  // aSet.size + bSet.size === mergedSet.size &&
-  //   console.log("countNumbersNotInB:", a.length, a, b);
-  // aSet.size + bSet.size > mergedSet.size &&
-  //   console.log(
-  //     "countNumbersNotInB:",
-  //     a.length - (a.length + bSet.size - halfMergedSet.size),
-  //     a,
-  //     b
-  //   );
-
-  // const bSet = new Set(b);
-  // const mergedSet = new Set([...bSet, ...a]);
-  // console.log(
-  //   "countNumbersNotInB:",
-  //   a.length - (bSet.size + a.length - mergedSet.size),
-  //   a,
-  //   b
-  // );
-  // a.length - ((a.length - aSet.size) + (aSet.size + bSet.size - mergedSet.size)),
+  return count;
 }
-countNumbersNotInB([3, 3, 5, 5, 7, 7], [1, 2, 3, 3, 5]);
+console.log("countNumbersNotInB:", countNumbersNotInB([3, 3, 5, 5, 7, 7], [1, 2, 3, 3, 5]));
+
+
+
+
+
+
 
 
 
